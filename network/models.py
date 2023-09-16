29c16ba2b4661378618b54cde7fc54373af12c0b
yamano <yamano@cb.com>
2023-09-16 19:51:20 +0000

    fixed user models bug

--- network/models.py
+++ network/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class User(AbstractUser):
-    following = models.ManyToManyField("self", related_name="follower")
+    following = models.ManyToManyField("self", related_name="follower", symmetrical=False)
     def __str__(self):
         return f"{self.username}"
 
